fce039ad5103651be1c381db20bde7279427a933
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-28 15:11:54 +0200

    add solution

--- server/app.js
+++ server/app.js
@@ -17,11 +17,57 @@ app.use(express.json());
 /*
   Endpoint to handle GET requests to the root URI "/"
 */
-app.get("/", (req, res) => {
-  res.json({
-    "/articles": "read and create new articles",
-    "/articles/:id": "read, update and delete an individual article",
+app.get("/articles", (req, res) => {
+  db.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.status(500);
+      res.send("Something went wrong");
+    });
+});
+
+app.post("/articles", (req, res) => {
+  db.insert(req.body)
+    .then((newPost) => {
+      res.status(201).send(newPost);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
+    });
+});
+//id
+app.get("/articles/:id", (req, res) => {
+  db.findById(req.params.id).then((data) => {
+    if (data) {
+      res.send(data);
+    } else {
+      console.log("Post not found");
+    }
   });
+});
+
+//patch
+
+app.patch("/articles/:id", (req, res) => {
+  db.updateById(req.params.id, req.body).then((updatedPost) => {
+    if (updatedPost) {
+      res.status(200).send(updatedPost);
+    } else {
+      res.status(404);
+      console.log("Not found");
+    }
+  });
+});
+
+app.delete("/articles/:id", (req, res) => {
+  db.deleteById(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => console.log("error"));
 });
 
 /*
